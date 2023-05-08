6fbdc1921998364b0907978038c3f66ea6307c64
Fatima Mohammed  Alzahrani <107775566+FatimaALzahrani@users.noreply.github.com>
2023-05-08 19:26:39 +0300

    Update bit++.py

--- implementation/Bit++/bit++.py
+++ implementation/Bit++/bit++.py
@@ -4,15 +4,13 @@ Link: https://codeforces.com/contest/282/problem/A
 Tags: implementation
 Complexity: O(n)
 """
-#طلب المستخدم يدخل عدد صحيح
 n = int(input())
 x = 0
-#فور للمرور بالقيم من 1 ل العدد المدخلمن الستخدم
 for _ in range(n):
-    oper = input()# نطلب المستخدم يدخل عملية
-    if oper[1] == '+':#اذا كانت العلامة اللي دخلها جميع زيد واحد
+    oper = input()
+    if oper[1] == '+':
         x += 1
     else:
-        x -= 1#اذا كانت العلامة طرح نقص واحد
+        x -= 1
 print(x)
 # Solved by: Muaath Alqarni
